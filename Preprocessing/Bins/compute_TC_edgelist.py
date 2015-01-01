b378fbc4e9b9322acf24ee4a638f03f86135ca5d
hongyusu <hongyu.su.uh@gmail.com>
2015-01-01 03:04:10 +0200

    automatic update github

---
+++ Preprocessing/Bins/compute_TC_edgelist.py
@@ -0,0 +1,26 @@
+
+
+import re
+
+def compute_edgelist(tcdbcollabFilename,EFilename):
+  fout = open(EFilename,'w')
+  index = 0
+  mapper = {}
+  res = []
+  for line in open(tcdbcollabFilename):
+    if line.startswith('TC__'):
+      index+=1
+      name = re.sub('TC__','',line.strip())
+      mapper[name] = index
+      names = name.split('.')
+      if len(names) < 2:
+        continue
+      else:
+        print '.'.join(names[:len(names)-1]),name
+        res.append('%d,%d' % (mapper['.'.join(names[:len(names)-1])],mapper[name]))
+  fout.write('\n'.join(res))
+  fout.close()
+
+
+if __name__ == '__main__':
+  compute_edgelist('../../Experiments/Data/tcdb.collab','../../Experiments/Data/tcdb.TC.E')
