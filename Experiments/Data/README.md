f75298c62d1e92934ed1528fa6b66f4da1903096
hongyusu <hongyu.su@me.com>
2015-07-16 12:08:54 +0300

    automatic update github

--- Experiments/Data/README.md
+++ Experiments/Data/README.md
@@ -5,7 +5,7 @@
 1. These files are not uploaded into Github due to the size limit of 100MB.
 
 1. Format:
-   1. First row, '0 feature_id1 feature_id2 ...'
-   2. Other rows, 'protein_id feature_id1_val feature_id2_val ...'
+   1. First row (header line), '0 feature_id1 feature_id2 ...'
+   2. Other rows (data), 'protein_id feature_id1_val feature_id2_val ...'
 
 1. File `./tcdb.TC` can be taken as the multiple labels of proteins. Features (labels) are generated from a hierarchical structure of transporter protein classification system (TCDB) where both leave nodes and internal nodes are included. 
--- Preprocessing/Bins/separate_different_features.py
+++ Preprocessing/Bins/separate_different_features.py
@@ -18,30 +18,46 @@ def process_one_prefix(prefix):
     index += 1
     if line.startswith(prefix):
       featureidlist.append(index)
-  print len(proteinidlist),prefix,len(featureidlist)
+  print len(proteinidlist),prefix,len(featureidlist),min(featureidlist),max(featureidlist)
   #
   data = {}
   for line in open('../../Preprocessing/Results/tcdbdata.mtx'):
     proteinid,featureid,score = line.strip().split(' ')
     proteinid = eval(proteinid)
     featureid = eval(featureid)
+    score = eval(score)
     if not proteinid in data:
       data[proteinid] = {}
-    if featureid in featureidlist:
-      data[proteinid][featureid] = score
+    if not featureid in featureidlist:
+      continue
+    else:
+      if prefix in ['TB__']:
+        if not featureid in data[proteinid]:
+          data[proteinid][featureid] = score
+        if data[proteinid][featureid] < score:
+          data[proteinid][featureid] = score
+      else:
+          if not featureid in data[proteinid]:
+            data[proteinid][featureid] = 0 
+          data[proteinid][featureid] += 1
   # write
   fout = open('../../Experiments/Data/tcdb.%s' % re.sub('__','',prefix),'w')
+  ## write header line
   fout.write('0 %s\n' % ' '.join(map(str,sorted(featureidlist))))
+  ## write data
   for proteinid in sorted(proteinidlist):
       fout.write('%d' % proteinid)
       if not proteinid in data:
-        fout.write('%s/n' % ' na'*len(featureidlist))
+        fout.write('%s/n' % ' 0'*len(featureidlist))
       else:
         for featureid in sorted(featureidlist):
           if not featureid in data[proteinid]:
-            fout.write(' na')
+            fout.write(' 0')
           else:
-            fout.write(' %s' % data[proteinid][featureid])
+            if prefix in ['TB__']:
+              fout.write(' %.2f' % data[proteinid][featureid])
+            else:
+              fout.write(' %d' % data[proteinid][featureid])
       fout.write('\n')
   fout.close()
      
