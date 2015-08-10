f3a137d857e81dc5261f728e794e848a5b8bb519
hongyusu <hongyu.su.uh@gmail.com>
2015-08-10 16:31:23 +0300

    automatic update github

--- Experiments/Bins/parallel_svm.py
+++ Experiments/Bins/parallel_svm.py
@@ -101,7 +101,8 @@ def run():
   suffix   = 'val'
   isTest   = '0'
   # iterate over the lists
-  xFilenameList         = ['../Data/tcdb.TB','../Data/tcdb.TICoils','../Data/tcdb.TIGene3D']
+  xFilenameList         = ['../Data/tcdb.TB','../Data/tcdb.TICoils','../Data/tcdb.TIGene3D','../Data/tcdb.TIHamap','../Data/tcdb.TIPANTHER','../Data/tcdb.TIPfam','../Data/tcdb.TIPhobius','../Data/tcdb.TIPIRSF','../Data/tcdb.TIPRINTS','../Data/tcdb.TIProDom','../Data/tcdb.TIProSitePatterns','../Data/tcdb.TIProSiteProfiles','../Data/tcdb.TISignalP_EUK','../Data/tcdb.TISignalP_GRAM_NEGATIVE','../Data/tcdb.TISignalP_GRAM_POSITIVE','../Data/tcdb.TISMART','../Data/tcdb.TISUPERFAMILY','../Data/tcdb.TITIGRFAM','../Data/tcdb.TITMHMM']
+  xFilenameList         = ['../Data/tcdb.TIHamap']
   yFilenameList         = ['../Data/tcdb.TC']
   labelIndexList        = xrange(1,numLabel+1)
   foldIndexList         = xrange(1,kFold+1) 
