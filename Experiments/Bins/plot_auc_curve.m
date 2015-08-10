227b2b773e18d8eb189a6576b88a12572148104a
hongyusu <hongyu.su.uh@gmail.com>
2015-08-10 12:12:45 +0300

    automatic update github

--- Experiments/Bins/plot_auc_curve.m
+++ Experiments/Bins/plot_auc_curve.m
@@ -7,7 +7,7 @@ function plot_auc_curve()
 
   figure1 = figure('visible','off');
   set(figure1, 'PaperUnits', 'centimeters');
-  set(figure1, 'Position',[0 0 50 50]);
+  set(figure1, 'Position',[0 0 500 500]);
               
   for i=1:size(xFilenameList,1)
     xFilename = xFilenameList{i};
@@ -16,7 +16,8 @@ function plot_auc_curve()
     plot(X(:,1),X(:,2));
     hold on;
   end
-  legend(xFilenameList);
+  lgd = legend(xFilenameList);
+  set(lgd, 'interpreter','latex','Position', [0.7,0.5,0.0,0.0]);
   xlabel('False positive rate'); 
   ylabel('True positive rate');
   title('ROC Curves')
