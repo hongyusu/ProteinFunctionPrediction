144768b24bb548abb9adc65bd8c0405b7d7ddfd5
hongyusu <hongyu.su.uh@gmail.com>
2015-08-25 10:10:10 +0300

    add normalization before centering

--- Experiments/BinsMKL/compute_kernels.m
+++ Experiments/BinsMKL/compute_kernels.m
@@ -1,9 +1,9 @@
 
 
 function compute_kernels()
+
   xFilenameList = {'../Data/tcdb.TC','../Data/tcdb.TB','../Data/tcdb.TICoils','../Data/tcdb.TIGene3D','../Data/tcdb.TIHamap','../Data/tcdb.TIPANTHER','../Data/tcdb.TIPfam','../Data/tcdb.TIPhobius','../Data/tcdb.TIPIRSF','../Data/tcdb.TIPRINTS','../Data/tcdb.TIProDom','../Data/tcdb.TIProSitePatterns','../Data/tcdb.TIProSiteProfiles','../Data/tcdb.TISignalP_EUK','../Data/tcdb.TISignalP_GRAM_NEGATIVE','../Data/tcdb.TISignalP_GRAM_POSITIVE','../Data/tcdb.TISMART','../Data/tcdb.TISUPERFAMILY','../Data/tcdb.TITIGRFAM','../Data/tcdb.TITMHMM'};
 
-  if 1==0
   for i = 1:length(xFilenameList)
     xFilename = xFilenameList{i};
     m = dlmread(xFilename);
@@ -13,25 +13,12 @@ function compute_kernels()
       m = m(:,mSum>2);
     end
     K = m*m';
-    K = centering(K);
+    K = normalization(K);
     xFilename
     size(K)
-    dlmwrite(sprintf('%s.K',xFilename),K);
-  end
+    dlmwrite(sprintf('%s.k',xFilename),K);
   end
 
-  
-  for i = 2:length(xFilenameList)
-    i
-    xFilename = xFilenameList{i};
-    if i==2
-      Kall = dlmread(sprintf('%s.K',xFilename));
-    else
-      Kall = Kall + dlmread(sprintf('%s.K',xFilename));
-    end
-  end
-  Kall = Kall/(size(xFilenameList,2)-1);
-  dlmwrite(sprintf('../Data/tcdb.all.K'),Kall);
 end
 
 
@@ -43,4 +30,11 @@ function Kc=centering(K)
   J = ones(ell,1) * D;
   Kc = K - J - J' + E * ones(ell, ell);
   
+end
+
+
+function Kn=normalization(K)
+
+  Kn = K ./ ( sqrt(diag(K)) * sqrt(diag(K))' );
+
 end
