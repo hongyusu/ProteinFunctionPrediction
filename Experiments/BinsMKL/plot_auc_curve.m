247f54b16be6f13189df4352283c1305ba8bc384
hongyusu <hongyu.su.uh@gmail.com>
2015-08-26 12:02:35 +0300

    automatic update github

--- Experiments/BinsMKL/plot_auc_curve.m
+++ Experiments/BinsMKL/plot_auc_curve.m
@@ -3,7 +3,7 @@ function plot_auc_curve()
 
   addpath('./savefigure/');
 
-  [xFilenameList,yFilenameList,auc,acc,f1,precision,recall] = textread('../ResultsMKL/results','%s %s %s %s %s %s %s','headerlines',1);
+  [xFilenameList,yFilenameList,auc,auprc,acc,f1,precision,recall] = textread('../ResultsMKL/results','%s %s %s %s %s %s %s %s','headerlines',1);
   
   figure1 = figure('visible','off');
   set(figure1, 'PaperUnits', 'centimeters');
@@ -22,6 +22,24 @@ function plot_auc_curve()
   title('ROC Curves')
   hold off;
   export_fig(sprintf('../PlotsMKL/auc.jpg'))
+  
+  figure1 = figure('visible','off');
+  set(figure1, 'PaperUnits', 'centimeters');
+  set(figure1, 'Position',[0 0 800 800]);
+  for i=1:size(xFilenameList,1)
+    xFilename = xFilenameList{i};
+    yFilename = yFilenameList{i};
+    [X] = dlmread(sprintf('../ResultsMKL/perf_pr_%s_%s',regexprep(xFilename,'.*/',''),regexprep(yFilename,'.*/','')));
+    plot(X(:,1),X(:,2));
+    hold on;
+  end
+  lgd = legend(xFilenameList);
+  set(lgd, 'interpreter','latex','Position', [0.7,0.5,0.0,0.0]);
+  xlabel('False positive rate'); 
+  ylabel('True positive rate');
+  title('ROC Curves')
+  hold off;
+  export_fig(sprintf('../PlotsMKL/auprc.jpg'))
 
 end
 
